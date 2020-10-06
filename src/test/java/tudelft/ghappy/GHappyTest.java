35186e6a6aa8eaae285490a4e8085b9d54126ea6
Marco Holzer <holzer.marco@gmail.com>
2020-10-06 23:15:59 +0200

    solved ghapp

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,27 @@
 package tudelft.ghappy;
 
-public class GHappyTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.leapyear.LeapYear;
+
+public class GHappyTest
+{
+    @Test
+    public void shortstring()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("gg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shortstringfalse()
+    {
+        GHappy gHappy = new GHappy();
+        boolean result = gHappy.gHappy("g");
+        Assertions.assertFalse(result);
+    }
+
+
+
 }
